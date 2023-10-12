2106b7ff6f2836c45dd681e7f42c9f3810228377
SherryWu178 <sherry.wu178@gmail.com>
2023-10-12 19:13:37 +0000

    complete

--- q1_q2_classification/feature_visualization.py
+++ q1_q2_classification/feature_visualization.py
@@ -14,46 +14,16 @@ from skimage.color import rgb2lab, deltaE_cie76
 from collections import defaultdict
 
 PATH = "/content/hw1/checkpoint-4(freezed weight)/checkpoint-model-epoch50.pth"
-
+CLASS_NAMES = ['aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus', 'car',
+                   'cat', 'chair', 'cow', 'diningtable', 'dog', 'horse', 'motorbike',
+                   'person', 'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor']
+                   
 def load_model(filename):
     return torch.load(filename)
 
-def visualize(args, model, test_loader):
-    # Determine the total number of samples in the test dataset
-    total_samples = len(test_loader.dataset)
-
-    # Set a random seed for reproducibility (optional)
-    torch.manual_seed(27)
-
-    # Randomly select 1000 indices from the test dataset
-    selected_indices = torch.randperm(total_samples)[:1000]
-
-    # Create a Subset of the test dataset containing the selected samples
-    from torch.utils.data import Subset
-    subset = Subset(test_loader.dataset, selected_indices)
-
-    # Create a DataLoader for the selected samples
-    selected_test_loader = DataLoader(subset, batch_size=test_loader.batch_size, shuffle=True)
-    
-    model = ResNet(20)
-    model.load_state_dict(torch.load(PATH))
-    finetuned_model = model.resnet
-    features = extract_features(finetuned_model, args.device, selected_test_loader)
-
 def extract_features(model, device, test_loader):
-    # ! Do not modify the code in this function
 
-    """
-    Evaluate the model with the given dataset
-    Args:
-         model (keras.Model): model to be evaluated
-         dataset (tf.data.Dataset): evaluation dataset
-    Returns:
-         AP (list): Average Precision for all classes
-         MAP (float): mean average precision
-    """
-    
-    
+
     with torch.no_grad():
         test_features = []
         gt_classes = []
@@ -92,8 +62,8 @@ def draw_plot(test_features, gt_classes):
 
  
     # extract x and y coordinates representing the positions of the images on T-SNE plot
-    tx = tsne[:, 0]
-    ty = tsne[:, 1]
+    tx = tsne_results[:, 0]
+    ty = tsne_results[:, 1]
     
     tx = scale_to_01_range(tx)
     ty = scale_to_01_range(ty)
@@ -103,13 +73,13 @@ def draw_plot(test_features, gt_classes):
 
     # Prepare colors for class labels
     class_colors = plt.cm.tab20(np.linspace(0, 1, 20))
+    class_colors_dic = {}
+    for i in range(20):
+        class_colors_dic[CLASS_NAMES[i]] = class_colors[i]
 
-    # Create a dictionary to store the mean color for each class
-    class_mean_colors = defaultdict(list)
 
     # Create a scatter plot with color-coded points
     plt.figure(figsize=(10, 8))
-    
     mean_colors = np.zeros((len(gt_classes), 4))  # Initialize a 2D array for colors (RGBA format)
 
     for i, class_label in enumerate(gt_classes):
@@ -123,7 +93,8 @@ def draw_plot(test_features, gt_classes):
 
     # Create a legend with class labels
     handles = [plt.Line2D([0], [0], marker='o', color='w', label=f'Class {label}', 
-                        markersize=10, markerfacecolor=color) for label, color in class_mean_colors.items()]
+                        markersize=10, markerfacecolor=color) for label, color in class_colors_dic.items()]
+    print(handles)
     plt.legend(handles=handles, title='Object Classes')
 
     # Add labels and title
@@ -142,6 +113,28 @@ def draw_plot(test_features, gt_classes):
     # Print a message indicating that the plot has been saved
     print(f"Plot saved as {file_path}")
 
+def visualize(args, model, test_loader):
+    # Determine the total number of samples in the test dataset
+    total_samples = len(test_loader.dataset)
+
+    # Set a random seed for reproducibility (optional)
+    torch.manual_seed(27)
+
+    # Randomly select 1000 indices from the test dataset
+    selected_indices = torch.randperm(total_samples)[:1000]
+
+    # Create a Subset of the test dataset containing the selected samples
+    from torch.utils.data import Subset
+    subset = Subset(test_loader.dataset, selected_indices)
+
+    # Create a DataLoader for the selected samples
+    selected_test_loader = DataLoader(subset, batch_size=test_loader.batch_size, shuffle=True)
+    
+    model = ResNet(20)
+    model.load_state_dict(torch.load(PATH))
+    finetuned_model = model.resnet
+    test_features, gt_classes = extract_features(finetuned_model, args.device, selected_test_loader)
+    draw_plot(test_features, gt_classes)
 
 if __name__ == "__main__":
     args = ARGS(
